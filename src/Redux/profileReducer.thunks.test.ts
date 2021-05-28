f1d67a80dd66ae286712161fe5b44c46f13bce1e
Stanislav <verbatim8872@gmail.com>
2021-05-29 00:00:27 +0300

    add all profileReducer thunks tests

--- src/Redux/profileReducer.thunks.test.ts
+++ src/Redux/profileReducer.thunks.test.ts
@@ -1,5 +1,5 @@
 import DAL from "../api/api"
-import { getProfile, getUserStatus, profileActions } from "./profileReducer"
+import { editProfileInfo, getProfile, getUserStatus, profileActions, setPhoto, updateStatus } from "./profileReducer"
 
 jest.mock('../api/api')
 const mockedDAL = DAL as jest.Mocked<typeof DAL>
@@ -47,3 +47,68 @@ it('getUserStatus thunk', async () => {
     expect(dispatchMock).toHaveBeenCalledWith(profileActions.setStatus(result))
 })
 
+it('updateStatus thunk', async () => {
+    const thunk = updateStatus('some status', '1')
+
+    const result = {
+        errors: [],
+        data: {
+            profile: 'some profile data',
+            resultCode: 0
+        }
+    }
+
+    //@ts-ignore
+    mockedDAL.profile.updateStatus.mockReturnValue(Promise.resolve(result))
+
+    await thunk(dispatchMock, getStateMock, {})
+
+    expect(dispatchMock).toBeCalledTimes(1)
+    expect(dispatchMock).toHaveBeenCalledWith(profileActions.setStatus('some status'))
+})
+
+it('setPhoto thunk', async () => {
+    const thunk = setPhoto('some photo file')
+
+    const result = {
+        errors: [],
+        data: {
+            data: {
+                photos: 'some photo file'
+            },
+            resultCode: 0
+        }
+    }
+
+    //@ts-ignore
+    mockedDAL.profile.setPhoto.mockReturnValue(Promise.resolve(result))
+
+    await thunk(dispatchMock, getStateMock, {})
+
+    expect(dispatchMock).toBeCalledTimes(1)
+
+    //@ts-ignore
+    expect(dispatchMock).toHaveBeenCalledWith(profileActions.setPhotoSuccess('some photo file'))
+})
+
+it('editProfileInfo thunk', async () => {
+    const thunk = editProfileInfo({}, '2')
+
+    const result = {
+        errors: [],
+        data: {
+            data: {},
+            resultCode: 0
+        }
+    }
+
+    //@ts-ignore
+    mockedDAL.profile.getProfile.mockReturnValue(Promise.resolve(result))
+    //@ts-ignore
+    mockedDAL.profile.editProfileInfo.mockReturnValue(Promise.resolve({data: { resultCode: 0 }}))
+
+    await thunk(dispatchMock, getStateMock, {})
+
+    expect(dispatchMock).toBeCalledTimes(1)
+    expect(dispatchMock).toHaveBeenCalledWith(profileActions.getUserProfile(result))
+})
