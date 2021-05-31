22581c5c5de7a8fd84396fc702ea87a1cafe5500
Stanislav <verbatim8872@gmail.com>
2021-05-31 09:16:22 +0300

    add dialogs selectors

---
+++ src/Redux/dialogsSelectors.ts
@@ -0,0 +1,5 @@
+import { AppStateType } from './reduxStore'
+
+export const usersSelector = (state: AppStateType) => state.dialogsPage.users
+export const messagesSelector = (state: AppStateType) => state.dialogsPage.messages
+export const newMessageTextSelector = (state: AppStateType) => state.dialogsPage.newMessageText
