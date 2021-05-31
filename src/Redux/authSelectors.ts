590f98beb59e1ac42817ddeaf44b5f93dc53f2ce
Stanislav <verbatim8872@gmail.com>
2021-05-31 09:14:52 +0300

    add authelectors

---
+++ src/Redux/authSelectors.ts
@@ -0,0 +1,7 @@
+import { AppStateType } from './reduxStore'
+
+export const idSelector = (state: AppStateType) => state.auth.id
+export const emailSelector = (state: AppStateType) => state.auth.email
+export const loginSelector = (state: AppStateType) => state.auth.login
+export const isAuthSelector = (state: AppStateType) => state.auth.isAuth
+export const captchaUrlSelector = (state: AppStateType) => state.auth.captchaUrl
