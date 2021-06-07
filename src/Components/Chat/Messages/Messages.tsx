ae8d9b166dbd552abb37915c2c4719d04ef07eaa
Stanislav <verbatim8872@gmail.com>
2021-06-07 11:46:05 +0300

    add autoscroll

--- src/Components/Chat/Messages/Messages.tsx
+++ src/Components/Chat/Messages/Messages.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { chatMessagesSelector } from '../../../Redux/selectors/chatSelectors'
 import Message from './Message/Message'
 import s from './Messages.module.scss'
 
 const Messages: FC = () => {
+    const messageRef = useRef<HTMLDivElement>(null)
+    const [autoScroll, setAutoScroll] = useState(true)
     const messages = useSelector(chatMessagesSelector)
 
     const messagesItems = messages.map((message, i) => <Message
@@ -14,9 +16,24 @@ const Messages: FC = () => {
         message={message.message}
     />)
 
+    const scrollHandler = (e:React.UIEvent) => {
+        if(Math.abs((e.currentTarget.scrollHeight - e.currentTarget.scrollTop) - e.currentTarget.clientHeight) < 100){
+            setAutoScroll(true)
+        }else{
+            setAutoScroll(false)
+        }
+    }
+
+    useEffect(() => {
+        if(autoScroll){
+            messageRef.current?.scrollIntoView({behavior: 'smooth'})
+        }
+    }, [messages])
+
     return (
-        <div className={s.messages}>
+        <div className={s.messages} onScroll={scrollHandler}>
             {messagesItems}
+            <div ref={messageRef}></div>
         </div>
     )
 }
